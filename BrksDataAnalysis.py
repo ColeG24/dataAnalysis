53ce900e00cb360e9455c073560dc2f562cb78ea
DESKTOP-ERS0J62\cole <cole.gordon57@gmail.com>
2016-11-19 23:17:12 -0700

    Much more modularized.. Working towards next step

--- BrksDataAnalysis.py
+++ BrksDataAnalysis.py
@@ -7,21 +7,70 @@ from DataRead.GoogleStockReader import GoogleStockReader
 with warnings.catch_warnings():
     warnings.filterwarnings("ignore",category=DeprecationWarning)
 import datetime
-from datetime import timedelta
 from dateutil import parser
 
 
 class Analysis:
 
-    def run(self):
-        start = datetime.datetime(2014, 1, 1)
-        end = datetime.date.today()
-        data = p.read_csv(GoogleStockReader.getStockUrl("MSI", start, end), parse_dates=True, names=['Date','Open','High','Low','Close','Volume'])
-        # cvData = p.read_csv("C:\\Users\\cole\\Desktop\\hw3\\crossValidationData.csv")
+    class StopLossLim:
+        def __init__(self, price):
+            self.price = price
 
+    class DataBlock:
+        def __init__(self, series, date):
+            self.series = series
+            self.date = date
 
+        def getRollingData(self, window, operation='mean'):
+            s = self.series.resample("1D").fillna('ffill').rolling(window=window)
+            if (operation == 'mean'):
+                return s.mean()[self.date]
+            if (operation == 'max'):
+                return s.max()[self.date]
+            if (operation == 'min'):
+                return s.min()[self.date]
+            if (operation == 'var'):
+                return s.var()[self.date]
 
-        # temp
+        def peaked(self, distBtwnMinAndMax, dataPieces, minWindow=30, maxWindow=20, peakWidthMax=20, peakHeightMin=5):
+            max = self.series.resample("1D").fillna('ffill').rolling(window=maxWindow).max()[self.date]
+            min = self.series.resample("1D").fillna('ffill').rolling(window=minWindow).min()[self.date]
+
+            index = 0  # index of current datablock
+            for i in range(0, len(dataPieces)):
+                if (dataPieces[i].date == self.date):
+                    index = i
+                    break
+
+            # Peak is tall enough
+            diff = max - min
+            if diff < peakHeightMin:
+                return False
+
+            # If is min
+            if min == self.open:
+                a = False
+                dist = index - distBtwnMinAndMax
+                for i in range(dist, index):
+                    if dataPieces[i].open == max:  # If max was within distBtwnMinAndMax
+                        a = True
+                if a == False:
+                    return False
+            else:
+                return False
+
+            # Peak is skinny enough
+            dist = index - distBtwnMinAndMax
+
+            for i in range(dist, index):
+                if dataPieces[i].open <= min <= dataPieces[i + 1].open:
+                    return True
+            return False
+
+    def __init__(self, company, start=datetime.datetime(2014, 1, 1), end=datetime.date.today()):
+
+        data = p.read_csv(GoogleStockReader.getStockUrl(company, start, end), parse_dates=True, names=['Date', 'Open', 'High', 'Low', 'Close', 'Volume'])
+
         d = [x for x in data['Date'].values]
         o = [x for x in data['Open'].values]
         h = [x for x in data['High'].values]
@@ -41,81 +90,12 @@ class Analysis:
             low.append(float(l[x]))
             close.append(float(c[x]))
 
-        class StopLossLim:
-            def __init__(self, price):
-                self.price = price
-
-        class DataBlock:
-            def __init__(self, series, date):
-                self.series = series
-                self.date = date
-
-            def getRollingData(self, window, operation='mean'):
-                s = self.series.resample("1D").fillna('ffill').rolling(window=window)
-                if (operation == 'mean'):
-                    return s.mean()[self.date]
-                if (operation == 'max'):
-                    return s.max()[self.date]
-                if (operation == 'min'):
-                    return s.min()[self.date]
-                if (operation == 'var'):
-                    return s.var()[self.date]
-
-            def peaked(self, distBtwnMinAndMax, dataPieces, minWindow=30, maxWindow=20, peakWidthMax = 20, peakHeightMin = 5):
-                max = self.series.resample("1D").fillna('ffill').rolling(window=maxWindow).max()[self.date]
-                min = self.series.resample("1D").fillna('ffill').rolling(window=minWindow).min()[self.date]
-
-                index = 0 # index of current datablock
-                for i in range(0, len(dataPieces)):
-                    if (dataPieces[i].date == self.date):
-                        index = i
-                        break
-
-                # Peak is tall enough
-                diff = max - min
-                if diff < peakHeightMin:
-                    return False
-
-                # If is min
-                if min == self.open:
-                    a = False
-                    dist = index - distBtwnMinAndMax
-                    for i in range(dist, index):
-                        if dataPieces[i].open == max: # If max was within distBtwnMinAndMax
-                            a = True
-                    if a == False:
-                        return False
-                else:
-                    return False
-
-
-                # Peak is skinny enough
-                dist = index - distBtwnMinAndMax
-
-                for i in range(dist, index):
-                    if dataPieces[i].open <= min <= dataPieces[i+1].open:
-                        return True
-                return False
-
-
-
-
-        # class DataPieces:
-        #     def __init__(self, series):
-        #         self.dataPoints = []
-        #         self.series = series
-        #         self.series.cumsum()
-        #
-        #     def addPoint(self, data_point):
-        #         self.dataPoints.append(data_point)
-        #         data_point
-
         dataPieces = []
         s = p.Series(open, dates)
         s.cumsum()
         # Initialize all basic data
         for dayIndex in range(0, len(dates)):
-            db = DataBlock(s, dates[dayIndex])
+            db = Analysis.DataBlock(s, dates[dayIndex])
             db.index = dayIndex
             db.open = open[dayIndex]
             db.high = high[dayIndex]
@@ -123,8 +103,17 @@ class Analysis:
             db.close = close[dayIndex]
             dataPieces.append(db)
 
+        dataPieces.reverse()
+        dates.reverse()
+        open.reverse()
+        self.dataPoints = dataPieces;
+        self.dates = dates
+        self.open = open
 
+    def run(self):
 
+        s = p.Series(self.open, self.dates)
+        s.cumsum()
 
         movAvg5 = s.resample("1D").fillna('ffill').rolling(window=5,)
         movAvg10 = s.resample("1D").fillna('ffill').rolling(window=10,)
@@ -134,9 +123,8 @@ class Analysis:
         movAvg160 = s.resample("1D").fillna('ffill').rolling(window=160,)
         movAvg320 = s.resample("1D").fillna('ffill').rolling(window=320,)
 
-        print(dataPieces[50].getRollingData(5))
-        # print(len(movAvg320))
-        print(len(dates))
+        print(self.dataPoints[50].getRollingData(5))
+        print(len(self.dates))
 
 
 
@@ -155,17 +143,14 @@ class Analysis:
         onlyBuyWhenNumIsAbove = 0
         onlySellWhenNumIsAbove = 0
 
-        dataPieces.reverse()
-        dates.reverse()
-
-        sll = StopLossLim(0)
+        sll = Analysis.StopLossLim(0)
 
         lastHigh = 0
 
         priceSoldAt = 0
 
-        for dayIndex in range(len(dataPieces)):
-            db = dataPieces[dayIndex]
+        for dayIndex in range(len(self.dataPoints)):
+            db = self.dataPoints[dayIndex]
 
             # Adjust StopLossLim
             if (stockOwned > 0):
@@ -273,7 +258,7 @@ class Analysis:
                 sellAmt = stockOwned * sll.price
                 balance += sellAmt
                 stockOwned = 0
-                if self.peakedRecently(db, dataPieces, dayIndex):
+                if self.peakedRecently(db, self.dataPoints, dayIndex):
                     plt.plot(db.date, db.open, marker='o', linestyle='--', color='g', markersize=30)
                 else:
                     plt.plot(db.date, db.open, marker='*', linestyle='--', color='g', markersize=30)
@@ -282,7 +267,7 @@ class Analysis:
             else:
                     print("no stock")
 
-        currentPrice = dataPieces[len(dataPieces)-1].open
+        currentPrice = self.dataPoints[len(self.dataPoints)-1].open
         stockMoney=stockOwned*currentPrice
         equity = balance + stockMoney
         profit = equity - initialBalance
@@ -299,6 +284,48 @@ class Analysis:
         plt.ylabel('some numbers')
         plt.show()
 
+    def movingAvgIndex(self, db):
+        # Within range to 20 day min, and its starting to pick up
+        goodBuyShortTerm = db.open * .98 <= db.getRollingData(20, 'min') and db.getRollingData(
+            50) > db.getRollingData(100)
+
+        # Within range to 100 day min, and its starting to pick up
+        goodBuyLongTerm = db.open * .98 <= db.getRollingData(100, 'min') and db.getRollingData(
+            100) > db.getRollingData(150)
+
+        # Price is well below usual
+        wellBelowUsual = (db.open * .97 < db.getRollingData(100, 'min') and db.getRollingData(
+            100) > db.getRollingData(150)) \
+                         or db.open * .98 < db.getRollingData(150, 'min')
+
+        # Most expensive its been in 20 days, and its slowing down
+        goodSellShortTerm = db.open * .99 >= db.getRollingData(20, 'max') and db.getRollingData(
+            5) < db.getRollingData(50)
+
+        # Most expensive its been in 50 days, and its slowing down
+        goodSellLongTerm = db.open * .98 >= db.getRollingData(50, 'max') and db.getRollingData(
+            10) < db.getRollingData(100)
+
+        # belowMarks = db.open <= db.getRollingData(60) #and db.getRollingData(3) < db.getRollingData(10)
+
+        num = 0
+        # Buy
+        if goodBuyShortTerm or goodBuyLongTerm or wellBelowUsual:
+            # A number that correlates with how strong the buy will be
+            num = ((db.getRollingData(50) / db.getRollingData(4)) - 1) * 100
+        # Sell
+        elif goodSellLongTerm or goodSellShortTerm:
+            # A number that correlates with how strong the sell will be
+            num = (db.getRollingData(15) / db.getRollingData(50) - 1) * 100
+        return num
+
+    # Stripped down run
+    def bare_run (self):
+        pass
+
+    def csv_indices(self):
+        # Take multiple indices and add them to a csv. This csv can then be used by neural network
+        pass
 
     def peakedRecently(self, db, dataPieces, index, peakLookBack=3):
 
@@ -317,5 +344,5 @@ class Analysis:
 
 
 if __name__ == "__main__":
-    A = Analysis()
+    A = Analysis("MSI")
     A.run()
