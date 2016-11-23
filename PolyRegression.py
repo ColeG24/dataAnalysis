53c45e60c28ddc25663b7bf9ac26d18a0bc92721
DESKTOP-ERS0J62\cole <cole.gordon57@gmail.com>
2016-11-22 18:38:35 -0700

    Cross validation stuff didnt get saved for whatever reason

--- PolyRegression.py
+++ PolyRegression.py
@@ -6,7 +6,7 @@ import  pandas as p
 
 degrees = [2, 3, 4, 5]
 data = p.read_csv("C:\\Users\\cole\\Desktop\\Brks\\MSITrain.csv")
-# cvData = p.read_csv("C:\\Users\\cole\\Desktop\\Brks\\MSICV.csv")
+cvData = p.read_csv("C:\\Users\\cole\\Desktop\\Brks\\MSI_CV.csv")
 
 X1 = [x for x in data['x1'].values]
 X2 = [x for x in data['x2'].values]
@@ -23,53 +23,40 @@ X11 = [x for x in data['x11'].values]
 Y1 = [x for x in data['y1'].values]
 Y2 = [x for x in data['y2'].values]
 
+cvX1 = [x for x in cvData['x1'].values]
+cvX2 = [x for x in cvData['x2'].values]
+cvX3 = [x for x in cvData['x3'].values]
+cvX4 = [x for x in cvData['x4'].values]
+cvX5 = [x for x in cvData['x5'].values]
+cvX6 = [x for x in cvData['x6'].values]
+cvX7 = [x for x in cvData['x7'].values]
+cvX8 = [x for x in cvData['x8'].values]
+cvX9 = [x for x in cvData['x9'].values]
+cvX10 = [x for x in cvData['x10'].values]
+cvX11 = [x for x in cvData['x11'].values]
 
-class T:
-    pass
-
+cvY1 = [x for x in cvData['y1'].values]
+cvY2 = [x for x in cvData['y2'].values]
 
 xVectors = []
-
 for i in range(0, len(X1)):
-    # t = T()
-    # t.x1 = X1[i]
-    # t.x2 = X2[i]
-    # t.x3 = X3[i]
-    # t.x4 = X4[i]
-    # t.x5 = X5[i]
-    # t.x6 = X6[i]
-    # t.x7 = X7[i]
-    # t.x8 = X8[i]
-    # t.x9 = X9[i]
-    # t.x10 = X10[i]
-    # t.x11 = X11[i]
-    xVectors.append(X1)
-
-    # xVectors.append(t)
+    xVectors.append([X1,X2,X3,X4,X5,X6,X7,X8,X9,X10,X11])
 
 yVectors = []
 for i in range(0, len(X1)):
-    # t = T()
-    # t.y1 = Y1[i]
-    # t.y2 = Y1[i]
-    yVectors.append(Y1)
+    yVectors.append([Y1,Y2])
+
+cv_xVectors = []
+for i in range(0, len(cvX1)):
+    cv_xVectors.append([cvX1,cvX2,cvX3,cvX4,cvX5,cvX6,cvX7,cvX8,cvX9,cvX10,cvX11])
+
+cv_yVectors = []
+for i in range(0, len(X1)):
+    cv_yVectors.append([cvY1,cvY2])
+
 degrees = [2]
 for i in range(len(degrees)):
 
-
-
-    # cvRows = []
-    #
-    # for i in range(0, len(X1)):
-    #     t = T()
-    #     t.y = Y[i]
-    #     t.x1 = X1[i]
-    #     t.x2 = X2[i]
-    #     t.x3 = X3[i]
-    #     cvRows.append(t)
-
-    # ax = plt.subplot(1, len(degrees), i + 1)
-    # plt.setp(ax, xticks=(), yticks=())
 
     polynomial_features = PolynomialFeatures(degree=degrees[i],
                                              include_bias=False)
@@ -78,13 +65,13 @@ for i in range(len(degrees)):
                          ("linear_regression", linear_regression)])
     pipeline.fit(xVectors, yVectors)
 
-    # print("------------------------------------------------")
-    # print(degrees[i])
-    # avgE = 0.0
-    # length = len(cvX)
-    # for x in range(0, length):
-    #     e = (pipeline.predict(cvX[x]) - cvY[x]) ** 2
-    #     avgE += e
-    # avgE /= length
-    # print(avgE)
-    # print("------------------------------------------------")
+    print("------------------------------------------------")
+    print(degrees[i])
+    avgE = 0.0
+    length = len()
+    for x in range(0, length):
+        e = (pipeline.predict(cv_xVectors) - cv_yVectors) ** 2
+        avgE += e
+    avgE /= length
+    print(avgE)
+    print("------------------------------------------------")
